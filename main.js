cf4e47f391f07aa0cc77e088f8d5e0949a091720
liams <princewill835@gmail.com>
2022-05-25 12:22:52 -0700

    hover effect commencement

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const container = document.querySelector("#container")
-
-console.log(box)
+function gridbuild(rows, cols){
+    container.style.setProperty("--grid-row", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for(c = 0; c < (rows * cols); c++){
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-items"
+    }
+}
+gridbuild(16, 16);
