436fe4bca5d155760b68070861f9bd23bbf24158
liams <princewill835@gmail.com>
2022-05-25 18:49:07 -0700

    working on the design

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const container = document.querySelector("#container")
-function gridbuild(rows, cols){
-    container.style.setProperty("--grid-row", rows);
-    container.style.setProperty("--grid-cols", cols);
-    for(c = 0; c < (rows * cols); c++){
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-items"
+function gridbuild(grid){
+    container.style.setProperty("--grid", grid);
+    for(c = 0; c < (grid * grid); c++){
+        var cell = document.createElement("div");
+        cell.className = "grid-items"
+        cell.addEventListener("mouseover", function(){
+            cell.style.backgroundColor = "black"
+        })
+        container.appendChild(cell)    
     }
 }
-gridbuild(16, 16);
+gridbuild(3);
+
+// container.addEventListener("mouseover", move) 
+const single = document.getElementsByClassName("grid-items")
+console.log(single)
+
